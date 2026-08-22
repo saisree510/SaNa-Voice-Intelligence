f387bd3f042d023a4dade6a856c017edd7617e2e
Sai Sree <saisree1231780@gmail.com>
2026-08-22 16:50:23 -0500

    Deploy canvas activity cards

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1488545867" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "62637218" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
