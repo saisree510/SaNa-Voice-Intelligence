240b6253fae78c475b9a0fadcf421ce3baf095f5
Sai Sree <saisree1231780@gmail.com>
2026-09-25 00:01:02 -0500

    fix: restore authenticated web build sessions

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"5a2a6a42cce67f965cf540fcecf616faca624a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3130506045" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1760730792" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
